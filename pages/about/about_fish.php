f282a1f8ef7edf10473890d6da597ef7fafc8e0e
Sylvain LACROIX <s.lacroix@onlineformapro.org>
2024-03-09 19:25:30 +0100

    mise en place de la page about

---
+++ pages/about/about_fish.php
@@ -0,0 +1,62 @@
+<?php session_start();
+require_once(__DIR__.'../../../assets/php/middleware/db_connect.php');
+require_once(__DIR__.'../../../assets/php/controllers/variables.php'); 
+$getData=$_GET;
+
+// on vérifie les données dans GET
+if(
+    !isset($getData['id']) ||
+    !is_numeric($getData['id'])
+){
+    echo('le poisson que vous cherchez s\'est échappé');
+    return;
+}
+
+//requête pour récupérer les données du poisson
+$retrieveFishs = $mysqlClient->prepare('SELECT * FROM fishs WHERE id=:id');
+$retrieveFishs->execute([
+    'id'=>(int)$getData['id'],
+]);
+$detailsfish=$retrieveFishs->fetchAll(PDO::FETCH_ASSOC);
+
+// vérification du résultat de la requette
+if($detailsfish===[]){
+    echo('le poisson est décroché');
+    return;
+}
+$fish=[
+    'id'=>$detailsfish[0]['id'],
+    'fish_name'=>$detailsfish[0]['fish_name'],
+    'average_size'=>$detailsfish[0]['average_size'],
+    'about'=>$detailsfish[0]['about'],
+    'image'=>$detailsfish[0]['image'],
+];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nom du poisson</title>
+    <link rel="stylesheet" href="/build/output.css">
+</head>
+<body class="bg-cover bg-center h-screen" style="background-image: url('../../assets/images/fonds-marins.webp'); ">
+    <?php require_once(dirname(__DIR__,2).'./assets/php/components/header.php' ); ?>
+    <main class="container mx-auto p-5">
+        <h1 class="text-center text-2xl mb-4 text-blue-50"></h1>
+        <section class="grid grid-cols-2 gap-3">
+            <div>
+                <h3 class="text-xl font-semibold mb-5"><?php echo $fish['fish_name']; ?> </h3>
+                <p class="mb-3"><?php echo $fish['about']; ?> </p>
+                <p><?php echo $fish['average_size']; ?> </p>
+            </div>
+            <div class="mx-auto">
+                <img class="rounded-xl" src="../../assets/.<?php echo $fish['image']; ?>" alt="<?php echo $fish['fish_name']; ?> ">
+            </div>
+        </section>
+    </main>
+</body>
+</html>
--- pages/home/home.php
+++ pages/home/home.php
@@ -12,7 +12,7 @@
     ?>
     <main class="container mx-auto p-5">
         <h1 class="text-center text-2xl font-bold">Poissons</h1>
-        <section class="grid grid-cols-5 gap-5">
+        <section class="md:grid grid-cols-3 xl:grid grid-col3 gap-5">
             <?php foreach ($fishs as $fish) : ?>
                 <article class="card rounded-xl overflow-hidden shadow-xl hover:shadow-2xl">
                     
@@ -21,11 +21,12 @@
                     <div class="px-6 py-4">
                         <h2 class="text-xl mb-2"><?php echo $fish['fish_name']; ?> </h2>
                         <p class="text-gray-700">
-                        <?php echo $fish['about'];?>
+                            Taille moyenne :
+                        <?php echo $fish['average_size'];?>
                         </p>
                     </div>
                     <div class="px-6 py-4">
-                        <a href="#" class="px-3 py-4 bg-gray-200 rounded-2xl">En savoir plus</a>
+                        <a href="/pages/about/about_fish.php?id=<?php echo($fish['id']); ?> " class="px-3 py-4 bg-gray-200 rounded-2xl">En savoir plus</a>
                     </div>
                 </article>
             <?php endforeach; ?>
